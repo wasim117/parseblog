fbe7c9f4d3af5137acd09c27d6af796f25520985
Wasim Shaikh <wasim117@gmail.com>
2015-10-14 22:09:24 +0530

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,14 @@
  Parse.initialize("28I2KtJC7OSfMX7gh9UN5SGiif61bpUBzG9pdq8Z", "bm8W75dpSmvayu26EWikgyq8s263log6HNl4FWTW");
-    var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
+var Blog = Parse.Object.extend("Blog");
+var Blogs = Parse.Collection.extend({
+    model: Blog
+});
+var blogs = new Blogs();
+blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
 });
