8cb7d172f393a1c641fb11c1c018b0fc0e3bd223
Wasim Shaikh <wasim.shaikh@thenewsmarket.com>
2016-01-26 00:41:31 +0530

    pup

--- js/blog.js
+++ js/blog.js
@@ -1,28 +1,28 @@
 $(function() {
 // Enable jQuery for Parse
-	Parse.$ = jQuery;
- Parse.initialize("28I2KtJC7OSfMX7gh9UN5SGiif61bpUBzG9pdq8Z", "bm8W75dpSmvayu26EWikgyq8s263log6HNl4FWTW");
- var Blog = Parse.Object.extend("Blog");
- var Blogs = Parse.Collection.extend({
-     model: Blog
- });
- var BlogsView =  Parse.View.extend({
-    template: Handlebars.compile($('#blogs-tpl').html()),
-    render: function(){ 
-        var collection = { blog: this.collection.toJSON() };
-        this.$el.html(this.template(collection));
-    }
-});
- var blogs = new Blogs();
- blogs.fetch({
-     success: function(blogs) {
-        console.log(blogs);
-        var blogsView = new BlogsView({ collection: blogs });
-	blogsView.render();
-	$('.main-container').html(blogsView.el);
-     },
-     error: function(blogs, error) {
-         console.log(error);
-     }
- });
+    Parse.$ = jQuery;
+    Parse.initialize("28I2KtJC7OSfMX7gh9UN5SGiif61bpUBzG9pdq8Z", "bm8W75dpSmvayu26EWikgyq8s263log6HNl4FWTW");
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+        model: Blog
+    });
+    var BlogsView =  Parse.View.extend({
+        template: Handlebars.compile($('#blogs-tpl').html()),
+        render: function(){ 
+            var collection = { blog: this.collection.toJSON() };
+            this.$el.html(this.template(collection));
+        }
+    });
+    var blogs = new Blogs();
+    blogs.fetch({
+        success: function(blogs) {
+            console.log(blogs);
+            var blogsView = new BlogsView({ collection: blogs });
+            blogsView.render();
+            $('.main-container').html(blogsView.el);
+        },
+        error: function(blogs, error) {
+            console.log(error);
+        }
+    });
 });
