b28b5efa0c5ab32ccb35d14a6970f0ced91037f2
Wasim Shaikh <wasim117@gmail.com>
2016-01-25 17:52:29 +0530

    Updated

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,18 @@
+$(function() {
+// Enable jQuery for Parse
+	Parse.$ = jQuery;
  Parse.initialize("28I2KtJC7OSfMX7gh9UN5SGiif61bpUBzG9pdq8Z", "bm8W75dpSmvayu26EWikgyq8s263log6HNl4FWTW");
-var Blog = Parse.Object.extend("Blog");
-var Blogs = Parse.Collection.extend({
-    model: Blog
-});
-var blogs = new Blogs();
-blogs.fetch({
-    success: function(blogs) {
-        console.log(blogs);
-    },
-    error: function(blogs, error) {
-        console.log(error);
-    }
+ var Blog = Parse.Object.extend("Blog");
+ var Blogs = Parse.Collection.extend({
+     model: Blog
+ });
+ var blogs = new Blogs();
+ blogs.fetch({
+     success: function(blogs) {
+         console.log(blogs);
+     },
+     error: function(blogs, error) {
+         console.log(error);
+     }
+ });
 });
